5c278a02bc904493898f81da93a066d17c631f36
KwameGH22 <adarkwa.y.k@gmail.com>
2024-02-17 09:32:12 +0000

    layout folder and rootlayout component added

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,9 +1,16 @@
 import React from 'react'
+import NavBar from '../components/NavBar'
+import HeroSection from '../components/HeroSection'
+import Cards from '../components/Cards'
+import YX1Earphones from '../components/YX1Earphones'
 
 const Homepage = () => {
   return (
     <div>
-      
+      <NavBar/>
+      <HeroSection/>
+      <Cards/>
+      <YX1Earphones/>
     </div>
   )
 }
